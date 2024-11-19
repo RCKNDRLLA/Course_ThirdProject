4b36a7a6d4ef85062a3e8bb9f9d0a9bb6926cad3
RCKNDRLLA <sokolov.yakov@inbox.ru>
2024-11-19 12:58:02 +0500

    4.1.2

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .forms import UserForm
+from django.http import HttpResponse
 
 def index(request):
-    userform = UserForm()
-    return render(request, 'index.html', {'form': userform})
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        return HttpResponse(f'<h2>Привет, {name}, твой возраст: {age}</h2>')
+    else:
+        userform = UserForm()
+        return render(request, 'index.html', {'form': userform})
+
