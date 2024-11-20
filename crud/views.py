90a9e96b630c323f9104b854b6702d12bd875abc
RCKNDRLLA <sokolov.yakov@inbox.ru>
2024-11-20 14:42:58 +0500

    4.2.2

--- crud/views.py
+++ crud/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from .models import Person
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 
+# Получение данных из БД
 def index(request):
     people = Person.objects.all()
     return render(request, 'index.html', {'people': people})
 
+#Сохранение данных в БД
 def create(request):
     if request.method == 'POST':
         person = Person()
@@ -13,3 +15,26 @@ def create(request):
         person.age = request.POST.get('age')
         person.save()
     return HttpResponseRedirect('/')
+
+#Изменение данных в БД
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.age = request.POST.get('age')
+            person.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
+
+#Удаление данных из БД
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect('/')
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
