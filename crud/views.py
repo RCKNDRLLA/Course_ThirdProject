816ba8214846a114efe706f443ae90ba4375ff88
RCKNDRLLA <sokolov.yakov@inbox.ru>
2024-11-20 10:30:15 +0500

    4.1.9

--- crud/views.py
+++ crud/views.py
@@ -3,14 +3,12 @@ from .forms import UserForm
 from django.http import HttpResponse
 
 def index(request):
+    userform = UserForm()
     if request.method == 'POST':
         userform = UserForm(request.POST)
         if userform.is_valid():
             name = userform.cleaned_data['name']
             return HttpResponse(f'<h2>Hello, {name}</h2>')
         else:
-            return HttpResponse('Invalid data')
-    else:
-        userform = UserForm()
-        return render(request, 'index.html', {'form': userform})
+            return render(request, 'index.html', {'form': userform})
 
